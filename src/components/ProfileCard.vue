36c9bc7ca5832e2b6f3de62761e32fd10f8674f9
Padminie Smith <p.smith.ux@gmail.com>
2020-04-29 16:42:56 -0400

    created json file for profile cards & profile card component

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,24 @@
+<template> 
+<div class="profilecard">
+    {{ test }}
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "profilecard",
+  components: {
+    
+  },
+  data() {
+    return {
+      test: "hi"
+    };
+  },
+  methods: {}
+
+};
+</script>
+
+<style scoped></style>
