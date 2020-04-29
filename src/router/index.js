bb2abcf5e487c411079a2f523d227c56ba2843a6
Padminie Smith <p.smith.ux@gmail.com>
2020-04-29 17:00:32 -0400

    Added Padmini View Page to test profile cards

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Padmini from "../views/Padmini.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/padmini",
+    name: "Padmini",
+    component: Padmini
   },
   {
     path: "/about",
---
+++ src/views/Padmini.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="padmini">
+        <h1>
+            <ProfileCard />
+        </h1>
+    </div>
+</template>
+
+<script>
+import ProfileCard from '../components/ProfileCard.vue'
+import { data } from "../locale/components/profileCard.js";
+export default {
+    name: "padmini",
+  components: {
+    ProfileCard,
+  },
+  data() {
+    return {
+      data,
+    };
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
