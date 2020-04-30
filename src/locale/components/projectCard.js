43d01aac7d376464383d0177914f206365abca36
Rbia <rbia.gilani@gmail.com>
2020-04-30 18:15:34 -0400

    added JSON data

---
+++ src/locale/components/projectCard.js
@@ -0,0 +1,19 @@
+export const data = 
+    [
+        {
+            name: "name",
+            description: "description",
+            image: "/path/img.png",
+            students: "student1, student2, student3",
+            link: "https://www.website.com",
+            mainColour: "#333"
+        },
+        {
+            name: "name",
+            description: "description",
+            image: "/path/img.png",
+            students: "student1, student2, student3",
+            link: "https://www.website.com",
+            mainColor: "#fff"
+        }
+    ]
