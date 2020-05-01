11e36960179a116ffd42d0dc0e743b2acd6d5ef8
eishikahoque <eishika.hoque@gmail.com>
2020-04-30 23:28:47 -0400

    feat. added styles & modal style

--- src/locale/components/profileCard.js
+++ src/locale/components/profileCard.js
@@ -1,15 +1,16 @@
 export const data = 
    [ 
         {
-            name: "Padmini",
-            title: "Awesome Coder",
-            color: "red"
-           
+            name: "Padmini Smith",
+            title: "Product Designer",
+            color: "#FDDDFF",
+            bio: "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
         },
         {
-            name: "Eishika",
-            title: "Super Awesome Designer",
-            color: "blue"
+            name: "Eishika Hoque Mostafiz",
+            title: "Front-End Developer",
+            color: "#C6EFFF",
+            bio: "I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences."
            
         },        
         
