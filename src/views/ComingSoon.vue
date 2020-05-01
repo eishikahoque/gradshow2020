0f350c33761962510043c7b6392625ecd4b581e4
Eileen Santiago <eileenmvs@gmail.com>
2020-05-01 14:11:51 -0400

    migrate to use bootstrap grid and update ComingSoon styles to use design system mixins

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -1,62 +1,51 @@
 <template>
-  <div>
+  <div class="container">
     <header>
-      <div class="container">
         <div class="row">
-          <div class="column">
+          <div class="col">
             <img src="../assets/images/imm-logo.svg" alt="" srcset="" />
           </div>
         </div>
-      </div>
     </header>
     <div class="intro-container">
       <!-- INTRO MESSAGE -->
-      <div class="row">
-        <div class="column">
-          <section>
-            <img
-              :src="require(`../assets/images/${images.comingSoon}`)"
-              alt="Coming Soon"
-            />
-          </section>
-        </div>
-      </div>
+      <img
+        class="coming-soon-header"
+        :src="require(`../assets/images/${images.comingSoon}`)"
+        alt="Coming Soon"
+      />
       <!-- NEWSLETTER -->
-      <div class="row newsletter">
-        <div class="column">
-          <!-- SIGNUP FORM -->
-          <div class="row row-no-padding">
-            <div class="column column-75">
-              <label for="email">Sign up here for more information</label>
-              <!-- INPUT TEXT -->
-              <input
-                name="email"
-                type="email"
-                placeholder="youremail@email.com"
-              />
-            </div>
-            <div class="column column-25">
-              <!-- SUBMIT BUTTON -->
-              <button class="primary subscribe-btn">Subscribe</button>
-            </div>
+      <div class="newsletter">
+        <!-- SIGNUP FORM -->
+        <label for="email">Sign up here for more information</label>
+        <div class="row no-gutters">
+          <div class="col-md-8">
+            <!-- INPUT TEXT -->
+            <input
+              name="email"
+              type="email"
+              placeholder="youremail@email.com"
+            />
+          </div>
+          <div class="col-md-4">
+            <!-- SUBMIT BUTTON -->
+            <button class="primary subscribe-btn">Subscribe</button>
           </div>
         </div>
       </div>
       <!-- FOOTER MESSAGE -->
       <div class="row footer-message">
-        <div class="column">
+        <div class="col">
           <p>Connect with your IMM grads on May 14th</p>
         </div>
       </div>
       <!-- BLOB CONTAINER -->
       <div class="blob-container"></div>
-      <!--  -->
     </div>
   </div>
 </template>
 
 <script>
-import "@/assets/css/gradshow.scss";
 
 export default {
   data: function() {
@@ -70,49 +59,58 @@ export default {
 </script>
 
 <style lang="scss">
+@import "@/styles/mixins/typography";
+@import "@/styles/mixins/buttons";
+@import "@/styles/mixins/breakpoints";
+
 header {
   margin: 70px 0;
 }
 
-// MEDIA QUERY FOR SCREENS LESS THAN 960px
-@media only screen and (max-width: 960px) {
-  header {
-    text-align: center;
-    margin: 70px 0;
-  }
+.coming-soon-header {
+  width: 100%;
+  max-width: 750px;
+  margin-bottom: 80px;
 }
+
 .intro-container {
   max-width: 800px;
   margin: 0 auto;
 }
+
 .newsletter {
   margin: 40px 0px;
 }
+
 .newsletter input[type="email"] {
-  background-color: #ffffff;
-  border-color: #ffffff;
+  @include text-h3();
+  width: 100%;
+  background-color: $color-white;
+  border-color: $color-white;
   height: 80px;
   border-radius: 24px 0px 0px 24px;
   padding: 0 3rem;
-  font-size: 2rem;
 }
+
 .newsletter label {
-  padding-left: 3rem;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 700;
+  @include text-h4();
+  margin-bottom: 15px;
+  display: block;
 }
+
 .subscribe-btn {
-  margin-top: 3rem;
+  @include text-h3();
+  color: $color-white;
   width: 100%;
   height: 80px;
   border-radius: 0px 24px 24px 0px;
-  font-size: 2rem;
   text-transform: unset;
+  @include primary-btn-color();
 }
 
 .footer-message {
-  text-align: center;
-  font-size: 2.4rem;
+  @include text-body-lg();
+  margin-top: 40px;
 }
 
 .blob-container {
@@ -125,22 +123,29 @@ header {
   background-image: url(../assets/images/blob.png);
   background-position: center;
   background-repeat: no-repeat;
-  //   background-size:;
 }
 
-// MEDIA QUERY FOR SCREENS LESS THAN 960px
-@media only screen and (max-width: 960px) {
-  .newsletter input[type="email"],
-  .subscribe-btn {
-    border-radius: 15px;
-    height: 50px;
-  }
-  .subscribe-btn {
-    margin: unset;
+@include media-breakpoint-down(sm) {
+  .newsletter input[type="email"], .subscribe-btn {
+    border-radius: 24px;
+    margin-bottom: 16px;
   }
   .newsletter label {
     text-align: center;
     padding-left: unset;
+  }
+  .footer-message {
+    text-align: center;
+  }
+}
+
+@include media-breakpoint-down(lg) {
+  header {
+    text-align: center;
+    margin: 70px 0;
+  }
+  .subscribe-btn {
+    margin: unset;
   }
 }
 </style>
