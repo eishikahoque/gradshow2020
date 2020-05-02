a6b5f454f8aaa7875dd883b9d7ed4b04d1e25021
Oby Okoye <okoyeevonne@gmail.com>
2020-05-02 17:45:50 -0400

    Added a user by oby

--- src/locale/components/profileCard.js
+++ src/locale/components/profileCard.js
@@ -181,43 +181,5 @@ export const data = [
 	},
 
 	{
-		id: 14,
-		name: "Eishika Hoque Mostafiz",
-		title: "Front-End Developer",
-		picture: "/static/images/eishika.png",
-		color: "#C6EFFF",
-		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
-		keywords: ["FE", "UXUI", "PD"],
-		portfolio: "https://eishika.com",
-		social: "https://www.linkedin.com/in/eishikahoque",
-		blurb:
-			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
-	},
-	{
-		id: 15,
-		name: "Padmini Smith",
-		title: "Product Designer",
-		picture: "/static/images/padmini.png",
-		color: "#FDDDFF",
-		hash: ["UXDesigner", "frontenddev", "projectmanager"],
-		keywords: ["UXUI", "FE", "PM"],
-		portfolio: "https://www.padminismith.com/",
-		social: "https://www.linkedin.com/in/padmini-smith",
-		blurb:
-			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
-	},
-
-	{
-		id: 16,
-		name: "Eishika Hoque Mostafiz",
-		title: "Front-End Developer",
-		picture: "/static/images/eishika.png",
-		color: "#C6EFFF",
-		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
-		keywords: ["FE", "UXUI", "PD"],
-		portfolio: "https://eishika.com",
-		social: "https://www.linkedin.com/in/eishikahoque",
-		blurb:
-			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
-	},
+	
 ];
