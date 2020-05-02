0f61ec07ba6665259c9517401e91d774b01006d3
Oby Okoye <okoyeevonne@gmail.com>
2020-05-02 17:40:22 -0400

    Added more users_Testing

--- src/locale/components/profileCard.js
+++ src/locale/components/profileCard.js
@@ -1,28 +1,223 @@
 export const data = [
-    {
-        id: 1,
-        name: "Padmini Smith",
-        title: "Product Designer",
-        picture: '/static/images/padmini.png',
-        color: "#FDDDFF",
-        hash: ["UXDesigner", "frontenddev", "projectmanager"],
-        keywords: ["UXUI", "FE", "PM"],
-        portfolio: "https://www.padminismith.com/",
-        social: "https://www.linkedin.com/in/padmini-smith",
-        blurb: "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
-    },
+	{
+		id: 1,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
 
-    {
-        id: 2,
-        name: "Eishika Hoque Mostafiz",
-        title: "Front-End Developer",
-        picture: '/static/images/eishika.png',
-        color: "#C6EFFF",
-        hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
-        keywords: ["FE", "UXUI", "PD"],
-        portfolio: "https://eishika.com",
-        social: "https://www.linkedin.com/in/eishikahoque",
-        blurb: "I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
-    },
+	{
+		id: 2,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
 
-]
+	{
+		id: 3,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 4,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+
+	{
+		id: 5,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 6,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+
+	{
+		id: 7,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 8,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+
+	{
+		id: 9,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 10,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+
+	{
+		id: 11,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 12,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+	{
+		id: 13,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 14,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+	{
+		id: 15,
+		name: "Padmini Smith",
+		title: "Product Designer",
+		picture: "/static/images/padmini.png",
+		color: "#FDDDFF",
+		hash: ["UXDesigner", "frontenddev", "projectmanager"],
+		keywords: ["UXUI", "FE", "PM"],
+		portfolio: "https://www.padminismith.com/",
+		social: "https://www.linkedin.com/in/padmini-smith",
+		blurb:
+			"Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development",
+	},
+
+	{
+		id: 16,
+		name: "Eishika Hoque Mostafiz",
+		title: "Front-End Developer",
+		picture: "/static/images/eishika.png",
+		color: "#C6EFFF",
+		hash: ["FrontEndDev", "UXDesigner", "ProductDesigner"],
+		keywords: ["FE", "UXUI", "PD"],
+		portfolio: "https://eishika.com",
+		social: "https://www.linkedin.com/in/eishikahoque",
+		blurb:
+			"I'm a creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences.",
+	},
+];
