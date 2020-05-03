ecb4438d6e0166659748c44a1dc52f3ca3ce3020
eishikahoque <eishika.hoque@gmail.com>
2020-05-03 00:49:17 -0400

    feat. alphabetized students

--- src/locale/components/profileCard.js
+++ src/locale/components/profileCard.js
@@ -1,48 +1,71 @@
 export const data = [
   {
     id: 1,
-    name: "Padmini Smith",
+    name: "Alena Kim",
     title: "Product Designer",
-    picture: "/static/images/padmini.png",
-    color: "#FDDDFF",
-    hash: ["#UXDesigner", "#frontenddev", "#projectmanager"],
-    keywords: ["UXUI", "FE", "PM"],
-    portfolio: "https://www.padminismith.com",
-    social: "www.linkedin.com/in/padmini-smith",
+    picture: "/static/images/alena.png",
+    color: "#C6EFFF",
+    hash: ["#ProductDesign", "#UXDesign", "#FrontEndDev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "https://www.alena.kim/",
+    social: "www.linkedin.com/in/alena-soojeong-kim",
     blurb:
-      "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
+      "I bridge delightful digital experiences and business goals through design. It is my goal to ease and enrich human-computer interactions."
   },
-
   {
     id: 2,
-    name: "Eishika Hoque Mostafiz",
-    title: "Front-End Developer",
-    picture: "/static/images/eishika.png",
-    color: "#C6EFFF",
-    hash: ["#FrontEndDev", "#UXDesigner", "#ProductDesigner"],
-    keywords: ["FE", "UXUI", "UXUI"],
-    portfolio: "https://eishika.com",
-    social: "www.linkedin.com/in/eishikahoque",
+    name: "Andrew Block",
+    title: "Front-end Developer",
+    picture: "/static/images/Andrew.png",
+    color: "#FDDDFF",
+    hash: ["#Developer", "#Advertising", "#Marketing"],
+    keywords: ["FE", "OT", "OT"],
+    portfolio: "http://www.andrewblock.site",
+    social: "www.linkedin.com/in/block-andrew/",
     blurb:
-      "A creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences."
+      "Advertisement Designer / Marketing Strategist with the ability to make ideas become successful realities."
   },
-
   {
     id: 3,
-    name: "Szerena Szabo",
-    title: "UX Designer",
-    picture: "/static/images/Szerna.png",
-    color: "#FDDDFF",
-    hash: ["#UXDesigner", "#DigitalCoordinator", "#Copywriter"],
-    keywords: ["UXUI", "OT", "OT"],
-    portfolio: "http://ux.szerenaszabo.com",
-    social: "www.linkedin.com/in/szerenaszabo",
+    name: "Ankit Gupta",
+    title: "Product Designer",
+    picture: "/static/images/ankit.png",
+    color: "#C6EFFF",
+    hash: ["#ProductDesign", "#UXDesign", "#FrontendDev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "http://ww.ankit.life",
+    social: "www.linkedin.com/in/ankit-raj-gupta",
     blurb:
-      "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
+      "I'm passionate about problem-solving. I use data and scientific methods to navigate my way and create accessible user experiences."
   },
-
   {
     id: 4,
+    name: "Chesney Gordon-Williams",
+    title: "Front-end Developer",
+    picture: "/static/images/Chesney.png",
+    color: "#FDDDFF",
+    hash: ["#UI/UXDesign", "#ProductDesign", "#FrontendDev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "http://www.chesmedia.com/",
+    social: "www.linkedin.com/in/chesneygordonwilliams",
+    blurb:
+      "As a UX Designer I'm looking forward to using everything I've learned to create and build more intuitive user experiences"
+  },
+  {
+    id: 5,
+    name: "Darren Vaithilingam",
+    title: "Graphic Designer",
+    picture: "/static/images/Darren.png",
+    color: "#C6EFFF",
+    hash: ["#FrontEndDev", "#GraphicDesign", "#AudioEngineer"],
+    keywords: ["FE", "UXUI", "OT"],
+    portfolio: "http://www.plungecreativemedia.com",
+    social: "www.linkedin.com/in/darrenvaithilingam",
+    blurb:
+      "My passion is to bring creations to life! With my skills in Audio, Video, Graphic, and now Web design, the ideas we can create is infinite!"
+  },
+  {
+    id: 6,
     name: "Diana Mantilla",
     title: "Digital Interactive Designer",
     picture: "/static/images/diana.png",
@@ -54,34 +77,6 @@ export const data = [
     blurb:
       "One of my passions in life is helping people by communicating ideas through good design and working with the community to create real social change."
   },
-
-  {
-    id: 5,
-    name: "Kai Scott",
-    title: "Game Developer",
-    picture: "/static/images/kai.png",
-    color: "#FDDDFF",
-    hash: ["#GameDev", "#FrontEndDev", "#PrototypeDesigner"],
-    keywords: ["OT", "FE", "UXUI"],
-    portfolio: "http://www.kailinmarie.com",
-    social: "www.linkedin.com/in/kai-marie-scott-64b2b0b2",
-    blurb: "An ocean of creativty and passion!"
-  },
-
-  {
-    id: 6,
-    name: "Shan Ruan",
-    title: "Front-End Developer",
-    picture: "/static/images/shan.png",
-    color: "#C6EFFF",
-    hash: ["#FrontEndDe", "#UXDesigner", "#BackEndDev"],
-    keywords: ["FE", "UXUI", "BE"],
-    portfolio: "http://shanruan.io",
-    social: "www.linkedin.com/in/shanruan",
-    blurb:
-      "I'm a developer and designer who motivated to integrate the art of programming with the beauty of design into web applications."
-  },
-
   {
     id: 7,
     name: "Drashya Gohil",
@@ -94,65 +89,71 @@ export const data = [
     social: "www.linkedin.com/in/drashya-gohil",
     blurb: "I play with emoti(c)ons, web and pen tool.  😄👨‍💻 ✒️"
   },
-
   {
     id: 8,
-    name: "Alena Kim",
-    title: "Product Designer",
-    picture: "/static/images/alena.png",
-    color: "#C6EFFF",
-    hash: ["#ProductDesign", "#UXDesign", "#FrontEndDev"],
+    name: "Eileen Santiago",
+    title: "User Experience Developer",
+    picture: "/static/images/Eileen.png",
+    color: "#FDDDFF",
+    hash: ["#UI/UXDesign", "#ProductDesign", "#FrontendDev"],
     keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "https://www.alena.kim/",
-    social: "www.linkedin.com/in/alena-soojeong-kim",
-    blurb:
-      "I bridge delightful digital experiences and business goals through design. It is my goal to ease and enrich human-computer interactions."
+    portfolio: "http://www.eileensantiago.com",
+    social: "www.linkedin.com/in/eileensantiago",
+    blurb: ""
   },
-
   {
     id: 9,
-    name: "Olivia Oleary",
-    title: "Front-end Developer",
-    picture: "/static/images/olivia.png",
-    color: "#FDDDFF",
-    hash: ["#DigitalDesigner", "#ProductDesigner", "#FrontEndDev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.oliviaoleary.com/",
-    social: "www.linkedin.com/in/oliviaoleary",
+    name: "Eishika Hoque Mostafiz",
+    title: "Front-End Developer",
+    picture: "/static/images/eishika.png",
+    color: "#C6EFFF",
+    hash: ["#FrontEndDev", "#UXDesigner", "#ProductDesigner"],
+    keywords: ["FE", "UXUI", "UXUI"],
+    portfolio: "https://eishika.com",
+    social: "www.linkedin.com/in/eishikahoque",
     blurb:
-      "A digital designer illustrator turned front-end developer, I like to design and code applications that will help others, but also have fun doing it!"
+      "A creative, big-picture thinker who loves crafting the little details. I have a deep sense of empathy, which helps me create inclusive, and meaningful user experiences."
   },
-
   {
     id: 10,
-    name: "Ankit Gupta",
-    title: "Product Designer",
-    picture: "/static/images/ankit.png",
-    color: "#C6EFFF",
-    hash: ["#ProductDesign", "#UXDesign", "#FrontendDev"],
+    name: "Flora Zuo",
+    title: "UI/UX Designer",
+    picture: "/static/images/Flora.png",
+    color: "#FDDDFF",
+    hash: ["#UI/UX Designer", "#Graphic Designer", "#Front-end Dev"],
     keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://ww.ankit.life",
-    social: "www.linkedin.com/in/ankit-raj-gupta",
+    portfolio: "http://www.flora-zzz.com",
+    social: "www.linkedin.com/in/flora-zuo-imm",
     blurb:
-      "I'm passionate about problem-solving. I use data and scientific methods to navigate my way and create accessible user experiences."
+      "From the moment I enter this world, I instinctively invent ways to have “fun”. I love creating new things that can “WOW” the world!!"
   },
-
   {
     id: 11,
-    name: "Rob Cottignies",
-    title: "UX Designer",
-    picture: "/static/images/Rob.png",
+    name: "Hira Fareed",
+    title: "Product Designer",
+    picture: "/static/images/Hira.png",
     color: "#FDDDFF",
-    hash: ["#UXDesigner", "#UXResearch", "#ProjectManager"],
-    keywords: ["UXUI", "UXUI", "PM"],
-    portfolio: "http://www.robcottignies.com",
-    social: "www.linkedin.com/in/rob-cottignies-guelph",
+    hash: ["#ProductDesigner", "#UI/UX Designer", "#Front-EndDev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "http://www.hirafareed.com",
+    social: "www.linkedin.com/in/hira-fareed",
     blurb:
-      "As a former Architectural Technician whose design is as robust as his user-focused principles, I want users to get what they need as fast as possible"
+      "A problem solver with a passion for creating experiences that are meaningful, user driven and delightful."
   },
-
   {
     id: 12,
+    name: "Kai Scott",
+    title: "Game Developer",
+    picture: "/static/images/kai.png",
+    color: "#FDDDFF",
+    hash: ["#GameDev", "#FrontEndDev", "#PrototypeDesigner"],
+    keywords: ["OT", "FE", "UXUI"],
+    portfolio: "http://www.kailinmarie.com",
+    social: "www.linkedin.com/in/kai-marie-scott-64b2b0b2",
+    blurb: "An ocean of creativty and passion!"
+  },
+  {
+    id: 13,
     name: "Lily Da Huang",
     title: "Full Stack Developer",
     picture: "/static/images/Lily.png",
@@ -165,33 +166,7 @@ export const data = [
       "UX/UI designer with 3+ years of experience. I have a passion for creating interactive digital products that empower people to achieve their goals. 💪"
   },
   {
-    id: 13,
-    name: "Yan Zhang",
-    title: "Product Designer",
-    picture: "/static/images/yan.png",
-    color: "#FDDDFF",
-    hash: ["#BackEndDev", "#FrontEndDev"],
-    keywords: ["BE", "FE"],
-    portfolio: "http://www.yanzhang.site",
-    social: "www.linkedin.com/in/getmelily",
-    blurb: ""
-  },
-
-  {
     id: 14,
-    name: "Yichen Li",
-    title: "Product UX/UI Designer",
-    picture: "/static/images/steve.png",
-    color: "#FDDDFF",
-    hash: ["#UX/UIdesig", "#Productdesign", "#front-enddev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.steveyichenli.com",
-    social: "www.linkedin.com/in/yichensteveli",
-    blurb: ""
-  },
-
-  {
-    id: 15,
     name: "Evonne Oby Okoye",
     title: "Creative Technologist",
     picture: "/static/images/Oby.png",
@@ -202,63 +177,34 @@ export const data = [
     social: "www.linkedin.com/in/iamoby",
     blurb: ""
   },
-
+  {
+    id: 15,
+    name: "Olivia Oleary",
+    title: "Front-end Developer",
+    picture: "/static/images/olivia.png",
+    color: "#FDDDFF",
+    hash: ["#DigitalDesigner", "#ProductDesigner", "#FrontEndDev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "http://www.oliviaoleary.com/",
+    social: "www.linkedin.com/in/oliviaoleary",
+    blurb:
+      "A digital designer illustrator turned front-end developer, I like to design and code applications that will help others, but also have fun doing it!"
+  },
   {
     id: 16,
-    name: "Darren Vaithilingam",
-    title: "Graphic Designer",
-    picture: "/static/images/Darren.png",
-    color: "#C6EFFF",
-    hash: ["#FrontEndDev", "#GraphicDesign", "#AudioEngineer"],
-    keywords: ["FE", "UXUI", "OT"],
-    portfolio: "http://www.plungecreativemedia.com",
-    social: "www.linkedin.com/in/darrenvaithilingam",
+    name: "Padmini Smith",
+    title: "Product Designer",
+    picture: "/static/images/padmini.png",
+    color: "#FDDDFF",
+    hash: ["#UXDesigner", "#frontenddev", "#projectmanager"],
+    keywords: ["UXUI", "FE", "PM"],
+    portfolio: "https://www.padminismith.com",
+    social: "www.linkedin.com/in/padmini-smith",
     blurb:
-      "My passion is to bring creations to life! With my skills in Audio, Video, Graphic, and now Web design, the ideas we can create is infinite!"
+      "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
   },
-
   {
     id: 17,
-    name: "Tim Lu",
-    title: "Front-end Developer",
-    picture: "/static/images/tim.png",
-    color: "#FDDDFF",
-    hash: ["#Front-endDev", "#GraphicDesigner", "#VideoProducer"],
-    keywords: ["FE", "UXUI", "OT"],
-    portfolio: "http://tim-lu-design.com/",
-    social: "www.linkedin.com/in/tim-tianjia-lu",
-    blurb: ""
-  },
-
-  {
-    id: 18,
-    name: "Yang Yang",
-    title: "Product Designer",
-    picture: "/static/images/Yang.png",
-    color: "#C6EFFF",
-    hash: ["#ProductDesigner", "#UI/UX Designer", "#Front-end Dev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://yangdesign.ca",
-    social: "www.linkedin.com/in/yangzm018",
-    blurb:
-      "I enjoy transferring people's needs to crafted pixels, and I believe aesthetics and human-centered design stand equally to create a better user experience."
-  },
-
-  {
-    id: 19,
-    name: "Eileen Santiago",
-    title: "User Experience Developer",
-    picture: "/static/images/Eileen.png",
-    color: "#FDDDFF",
-    hash: ["#UI/UXDesign", "#ProductDesign", "#FrontendDev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.eileensantiago.com",
-    social: "www.linkedin.com/in/eileensantiago",
-    blurb: ""
-  },
-
-  {
-    id: 20,
     name: "Rbia Gilani",
     title: "Product Designer",
     picture: "/static/images/rbia.png",
@@ -271,58 +217,91 @@ export const data = [
       "Using my passion for UX design and creative problem solving, I aim to develop applications that provide positive and meaningful user experiences"
   },
   {
-    id: 21,
-    name: "Andrew Block",
-    title: "Front-end Developer",
-    picture: "/static/images/Andrew.png",
+    id: 18,
+    name: "Rob Cottignies",
+    title: "UX Designer",
+    picture: "/static/images/Rob.png",
     color: "#FDDDFF",
-    hash: ["#Developer", "#Advertising", "#Marketing"],
-    keywords: ["FE", "OT", "OT"],
-    portfolio: "http://www.andrewblock.site",
-    social: "www.linkedin.com/in/block-andrew/",
+    hash: ["#UXDesigner", "#UXResearch", "#ProjectManager"],
+    keywords: ["UXUI", "UXUI", "PM"],
+    portfolio: "http://www.robcottignies.com",
+    social: "www.linkedin.com/in/rob-cottignies-guelph",
     blurb:
-      "Advertisement Designer / Marketing Strategist with the ability to make ideas become successful realities."
+      "As a former Architectural Technician whose design is as robust as his user-focused principles, I want users to get what they need as fast as possible"
   },
-
+  {
+    id: 19,
+    name: "Shan Ruan",
+    title: "Front-End Developer",
+    picture: "/static/images/shan.png",
+    color: "#C6EFFF",
+    hash: ["#FrontEndDe", "#UXDesigner", "#BackEndDev"],
+    keywords: ["FE", "UXUI", "BE"],
+    portfolio: "http://shanruan.io",
+    social: "www.linkedin.com/in/shanruan",
+    blurb:
+      "I'm a developer and designer who motivated to integrate the art of programming with the beauty of design into web applications."
+  },
+  {
+    id: 20,
+    name: "Yichen Li",
+    title: "Product UX/UI Designer",
+    picture: "/static/images/steve.png",
+    color: "#FDDDFF",
+    hash: ["#UX/UIdesig", "#Productdesign", "#front-enddev"],
+    keywords: ["UXUI", "UXUI", "FE"],
+    portfolio: "http://www.steveyichenli.com",
+    social: "www.linkedin.com/in/yichensteveli",
+    blurb: ""
+  },
+  {
+    id: 21,
+    name: "Szerena Szabo",
+    title: "UX Designer",
+    picture: "/static/images/Szerna.png",
+    color: "#FDDDFF",
+    hash: ["#UXDesigner", "#DigitalCoordinator", "#Copywriter"],
+    keywords: ["UXUI", "OT", "OT"],
+    portfolio: "http://ux.szerenaszabo.com",
+    social: "www.linkedin.com/in/szerenaszabo",
+    blurb:
+      "Event planner turned designer focused on user experience & project management, and skilled in front-end & back-end development"
+  },
   {
     id: 22,
-    name: "Hira Fareed",
-    title: "Product Designer",
-    picture: "/static/images/Hira.png",
+    name: "Tim Lu",
+    title: "Front-end Developer",
+    picture: "/static/images/tim.png",
     color: "#FDDDFF",
-    hash: ["#ProductDesigner", "#UI/UX Designer", "#Front-EndDev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.hirafareed.com",
-    social: "www.linkedin.com/in/hira-fareed",
-    blurb:
-      "A problem solver with a passion for creating experiences that are meaningful, user driven and delightful."
+    hash: ["#Front-endDev", "#GraphicDesigner", "#VideoProducer"],
+    keywords: ["FE", "UXUI", "OT"],
+    portfolio: "http://tim-lu-design.com/",
+    social: "www.linkedin.com/in/tim-tianjia-lu",
+    blurb: ""
   },
-
+  {
+    id: 23,
+    name: "Yan Zhang",
+    title: "Product Designer",
+    picture: "/static/images/yan.png",
+    color: "#FDDDFF",
+    hash: ["#BackEndDev", "#FrontEndDev"],
+    keywords: ["BE", "FE"],
+    portfolio: "http://www.yanzhang.site",
+    social: "www.linkedin.com/in/getmelily",
+    blurb: ""
+  },
   {
     id: 24,
-    name: "Flora Zuo",
-    title: "UI/UX Designer",
-    picture: "/static/images/Flora.png",
-    color: "#FDDDFF",
-    hash: ["#UI/UX Designer", "#Graphic Designer", "#Front-end Dev"],
+    name: "Yang Yang",
+    title: "Product Designer",
+    picture: "/static/images/Yang.png",
+    color: "#C6EFFF",
+    hash: ["#ProductDesigner", "#UI/UX Designer", "#Front-end Dev"],
     keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.flora-zzz.com",
-    social: "www.linkedin.com/in/flora-zuo-imm",
+    portfolio: "http://yangdesign.ca",
+    social: "www.linkedin.com/in/yangzm018",
     blurb:
-      "From the moment I enter this world, I instinctively invent ways to have “fun”. I love creating new things that can “WOW” the world!!"
-  },
-
-  {
-    id: 25,
-    name: "Chesney Gordon-Williams",
-    title: "Front-end Developer",
-    picture: "/static/images/Chesney.png",
-    color: "#FDDDFF",
-    hash: ["#UI/UXDesign", "#ProductDesign", "#FrontendDev"],
-    keywords: ["UXUI", "UXUI", "FE"],
-    portfolio: "http://www.chesmedia.com/",
-    social: "www.linkedin.com/in/chesneygordonwilliams",
-    blurb:
-      "As a UX Designer I'm looking forward to using everything I've learned to create and build more intuitive user experiences"
+      "I enjoy transferring people's needs to crafted pixels, and I believe aesthetics and human-centered design stand equally to create a better user experience."
   }
 ];
