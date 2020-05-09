0a4f048323814db6b92443de01259587e0580862
Oby Okoye <okoyeevonne@gmail.com>
2020-05-09 01:30:19 -0400

    Update projectCard.js

--- src/locale/components/projectCard.js
+++ src/locale/components/projectCard.js
@@ -1,4 +1,4 @@
-export const data = 
+export const data =
     [
         {
             id: 1,
@@ -16,24 +16,110 @@ export const data =
             image: "/static/images/projectImages/motiful.svg",
             students: "Padmini, Diana, Rob, & Lily",
             link: "https://huang23.dev.fast.sheridanc.on.ca/motiful/",
-            keywords: ["ar/vr"]
+            keywords: ["mobile/web app"]
         },
         {
             id: 3,
-            name: "Motiful",
-            description: "Motiful is an App to help brighten your day. Connect with other users to send & receive a positive motivational message & bring a little additional joy to your life at anytime on Mobile!",
-            image: "/static/images/projectImages/motiful.svg",
-            students: "Padmini, Diana, Rob, & Lily",
-            link: "https://huang23.dev.fast.sheridanc.on.ca/motiful/",
-            keywords: ["ar/vr"]
+            name: "SSL Jewelry",
+            description: "S&S Jewelry is a fully functional e-commerce that works seamlessly across all devices with integrated shopping carts, secure payment gateways, seamless navigation, clutter-free design and an AR try-on service.",
+            image: "/static/images/projectImages/ssljewelry.svg",
+            students: "Shan Ruan, Steve Yichen Li, Lily Da Huang",
+            link: "https://ssjewelry.shanruan.io/",
+            keywords: ["mobile/web app"]
         },
         {
             id: 4,
             name: "Project ADAO",
-            description: "Motiful is an App to help brighten your day. Connect with other users to send & receive a positive motivational message & bring a little additional joy to your life at anytime on Mobile!",
-            image: "/static/images/projectImages/motiful.svg",
+            description: "Project Adao is a rapid prototyping tool aiming to help reduce the early grunt-work of design. The prototype was built with Tensorflow, Teachable Machine, Vue and Vuetify components.",
+            image: "/static/images/projectImages/adao.svg",
             students: "Padmini, Diana, Rob, & Lily",
-            link: "https://huang23.dev.fast.sheridanc.on.ca/motiful/",
+            link: "https://www.projectadao.com/",
+            keywords: ["mobile/web app"]
+        },
+        {
+            id: 5,
+            name: "HoliVR",
+            description: "A virtual reality experience built with A-Frame, a framework based on ThreeJS, and Google Cardboard that allows users to experience the Indian Hindu festival of colors, Holi.",
+            image: "/static/images/projectImages/holivr.svg",
+            students: "Diana Mantilla, Eishika Hoque, Hira Fareed, Olivia OLeary, Rbia Gilani",
+            link: "https://holi-vr.netlify.app/",
             keywords: ["ar/vr"]
+        },
+        {
+            id: 6,
+            name: "Groovy Rain",
+            description: "a projected light installation that uses HeavyM projection mapping software to add a wide range of light effects to an otherwise static sculpture based on the song being played",
+            image: "/static/images/projectImages/groovyrain.svg",
+            students: "Padmini Smith, Rob Cottignies, Szerena Szabo, Alena Kim, & Andrew Block",
+            link: "https://www.behance.net/gallery/96706391/Groovy-Rain-A-HeavyM-Projection-Mapping-Project",
+            keywords: ["installation"]
+        },
+
+        {
+            id: 7,
+            name: "Robot Space",
+            description: "Motiful is an App to help brighten your day. Connect with other users to send & receive a positive motivational message & bring a little additional joy to your life at anytime on Mobile!",
+            image: "/static/images/projectImages/robotspace.svg",
+            students: "Tim, Yan, Flora",
+            link: "http://tim-lu-design.com/robot%20space/robot%20space.html",
+            keywords: ["ar/vr"]
+        },
+        {
+            id: 8,
+            name: "Project RAVE",
+            description: "an interactive sound installation built with Arduino, sound & light sensors.",
+            image: "/static/images/projectImages/projectrave.svg",
+            students: "Padmini Smith, & Evonne Oby Okoye",
+            link: "https://www.behance.net/gallery/96590803/Project-Rave-An-Interactive-Sound-Installation",
+            keywords: ["installation"]
+        },
+        {
+            id: 9,
+            name: "Lookey",
+            description: "A dynamic grocery product search App where you can search and place orders for products from multiple grocers in your area.",
+            image: "/static/images/projectImages/lookey.svg",
+            students: "Tim,Yan,Flora",
+            link: "https://www.youtube.com/watch?time_continue=11&v=H37BwTgwDhw&feature=emb_logo",
+            keywords: ["ar/vr"]
+        },
+        {
+            id: 10,
+            name: "Tale of two Strains - Website Redesign",
+            description: "A fun little website user flow / interaction comparison - We asked the user to do a task on both sites, assessed what site was more difficult to navigate, and redesigned accordingly!",
+            image: "/static/images/projectImages/talestrains.svg",
+            students: "Padmini Smith, Olivia Oleary, Diana Mantilla, & Yang Yang",
+            link: "https://www.behance.net/gallery/96570113/Tale-of-two-strains-Case-Study",
+            keywords: ["ar/vr"]
+        },
+        {
+            id: 11,
+            name: "PlantMe",
+            description: "An e-commerce website that offers personalized plant subscription boxes and provides detailed plant-care instructions.",
+            image: "/static/images/projectImages/plantme.svg",
+            students: "Padmini Smith, Steve Yichen Li, Shan Ruan, Lily Da Huang",
+            link: "https://www.steveyichenli.com/plantme",
+            keywords: ["mobile/web app"]
+        },
+        {
+            id: 12,
+            name: "Grocereasy",
+            description: "A React mobile app designed to make online grocery shopping more convenient and accessible for users who are on the go.",
+            image: "/static/images/projectImages/grocereasy.svg",
+            students: "Eishika Hoque, Hira Fareed, Rbia Gilani",
+            link: "http://grocereasy.eishika.com/",
+            keywords: ["mobile/web app"]
+        },
+        {
+            id: 13,
+            name: "Gaming Friend Finder",
+            description: "Love multiplayer games but don't have the squad?! Gaming Friend Finder can help... Built to connect like-minded gamers when looking for playing partners, this was an 8642 prototyping exercise with animated pitch materials..",
+            image: "/static/images/projectImages/gamingFriend.svg",
+            students: "Rob Cottignies, Drashya Gohil, Ankit Gupta, Andrew Block, & Padmini Smith",
+            link: "http://www.robcottignies.com/projects/Gaming-Friend-Finder-Project",
+            keywords: ["mobile/web app"]
         }
+
+
+
+
     ]
