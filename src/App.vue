af1173599ec8b5ae0fe5a106d447e4ce28827da5
Olivia OLeary <olearyo@sheridancollege.ca>
2020-05-12 18:35:29 -0400

    Change bg color + footer bg

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- MAIN BODY -->
     <router-view />
         <div class="bg-gradient"></div>
-
+      <!-- <div id="footer-bg"/> -->
   </div>
 </template>
 
@@ -19,11 +19,16 @@ export default {
 
 <style lang="scss">
   #app {
-  background-image: url(/static/images/homeBackgrounds/bg-no-shadow.png);
-  background-position: top;
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-size: cover;
-  
+    background-image: url(/static/images/homeBackgrounds/bg-no-shadow.png);
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;  
   } 
+
+  // #footer-bg {
+  //   background-image: url(/static/images/homeBackgrounds/footer.svg);
+  //   background-position: bottom;
+  //   background-size: 100%;
+  //   background-repeat: no-repeat;  
+  // } 
 </style>
