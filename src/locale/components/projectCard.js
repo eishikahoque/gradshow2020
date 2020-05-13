07a3af99d63065e0ddc3473e6a114adc8df314d0
Olivia OLeary <olearyo@sheridancollege.ca>
2020-05-13 14:38:24 -0400

    edit JSON object & images

--- src/locale/components/projectCard.js
+++ src/locale/components/projectCard.js
@@ -3,7 +3,7 @@ export const data =
         {
             id: 1,
             name: "YODAE",
-            description: "It’s an experimental project that uses Google Speech API and Natural Language API to interpret voice commands and render doodles using Quick,Draw! database. ",
+            description: "Yodae is an experimental project that uses Google's Speech & Natural Language API's to interpret voice commands and render doodles using the Quick,Draw! database.",
             image: "/static/images/projectImages/edited/yodae.png",
             students: "Yang Yang, Evonne Oby Okoye, Drashya Gohil, Ankit Gupta, & Eileen Santiago",
             link: "https://www.theyodae.com",
@@ -20,38 +20,36 @@ export const data =
         },
         {
             id: 3,
-            name: "SSL Jewelry",
-            description: "S&S Jewelry is a fully functional e-commerce that works seamlessly across all devices with integrated shopping carts, secure payment gateways, seamless navigation, clutter-free design and an AR try-on service.",
+            name: "S&S Jewelry",
+            description: "S&S Jewelry is a fully functional e-commerce site that works seamlessly across all devices with integrated shopping carts, secure payment gateways, seamless navigation, clutter-free design and an AR try-on service.",
             image: "/static/images/projectImages/edited/sns.png",
-            students: "Shan Ruan, Steve Yichen Li, Lily Da Huang",
+            students: "Shan Ruan, Steve Yichen Li, & Lily Da Huang",
             link: "https://ssjewelry.shanruan.io/",
             keywords: ["app"]
         },
         {
             id: 4,
             name: "Project ADAO",
-            description: "Project Adao is a rapid prototyping tool aiming to help reduce the early grunt-work of design. The prototype was built with Tensorflow, Teachable Machine, Vue and Vuetify components.",
+            description: "Project Adao is a rapid prototyping tool that helps reduce the early grunt-work of design. The prototype was built with Tensorflow, Teachable Machine, Vue and Vuetify components.",
             image: "/static/images/projectImages/edited/adao.png",
-            students: "Andrew Block, Drashya Gohil, Ankit Gupta, Olivia OLeary",
+            students: "Andrew Block, Drashya Gohil, Ankit Gupta, & Olivia OLeary",
             link: "https://www.projectadao.com/",
             keywords: ["app"]
         },
         {
             id: 5,
             name: "HoliVR",
-            description: "A virtual reality experience built with A-Frame, a framework based on ThreeJS, and Google Cardboard that allows users to experience the Indian Hindu festival of colors, Holi.",
+            description: "HoliVR is a virtual reality experience built with A-Frame - a framework based on ThreeJS - and Google Cardboard that allows users to experience the Indian Hindu festival of colors, Holi.",
             image: "/static/images/projectImages/edited/holivr.png",
-            students: "Diana Mantilla, Eishika Hoque, Hira Fareed, Olivia OLeary, Rbia Gilani",
+            students: "Diana Mantilla, Eishika Hoque, Hira Fareed, Olivia OLeary, & Rbia Gilani",
             link: "https://holi-vr.netlify.app/",
             keywords: ["ar/vr"]
         },
 
-
-
         {
             id: 6,
             name: "Project RAVE",
-            description: "an interactive sound installation built with Arduino, sound & light sensors.",
+            description: "Project Rave is an interactive sculpture that utilizes Arduino, and sound & light sensors to create a sound sculpture that lights up at different frequencies based on the music being played.",
             image: "/static/images/projectImages/edited/rave.png",
             students: "Padmini Smith, & Evonne Oby Okoye",
             link: "https://www.behance.net/gallery/96590803/Project-Rave-An-Interactive-Sound-Installation",
@@ -60,16 +58,16 @@ export const data =
         {
             id: 7,
             name: "Lookey",
-            description: "A dynamic grocery product search App where you can search and place orders for products from multiple grocers in your area.",
+            description: "Lookey is a dynamic grocery product search App where you can search and place orders for products from multiple grocers in your area.",
             image: "/static/images/projectImages/edited/lookey.png",
-            students: "Tim,Yan,Flora",
+            students: "Tim Lu, Yan Zhang, & Flora Zuo",
             link: "https://www.youtube.com/watch?time_continue=11&v=H37BwTgwDhw&feature=emb_logo",
             keywords: ["app"]
         },
         {
             id: 8,
-            name: "Tale of two Strains - Website Redesign",
-            description: "A fun little website user flow / interaction comparison - We asked the user to do a task on both sites, assessed what site was more difficult to navigate, and redesigned accordingly!",
+            name: "A Tale of two Strains",
+            description: "A fun little website user flow / interaction comparison - We asked our user to do a task on both sites, assessed what site was more difficult to navigate, and redesigned accordingly!",
             image: "/static/images/projectImages/edited/twostrains.png",
             students: "Padmini Smith, Olivia Oleary, Diana Mantilla, & Yang Yang",
             link: "https://www.behance.net/gallery/96570113/Tale-of-two-strains-Case-Study",
@@ -78,29 +76,37 @@ export const data =
         {
             id: 9,
             name: "PlantMe",
-            description: "An e-commerce website that offers personalized plant subscription boxes and provides detailed plant-care instructions.",
+            description: "PlantMe is an e-commerce website that offers personalized plant subscription boxes based on your lifestyle.",
             image: "/static/images/projectImages/edited/plantme.png",
-            students: "Padmini Smith, Steve Yichen Li, Shan Ruan, Lily Da Huang",
+            students: "Padmini Smith, Steve Yichen Li, Shan Ruan, & Lily Da Huang",
             link: "https://www.steveyichenli.com/plantme",
             keywords: ["app"]
         },
         {
             id: 10,
             name: "Grocereasy",
-            description: "A React mobile app designed to make online grocery shopping more convenient and accessible for users who are on the go.",
+            description: "Grocereasy is a React mobile app designed to make online grocery shopping more convenient and accessible for users who are on the go.",
             image: "/static/images/projectImages/edited/grocereasy.png",
-            students: "Eishika Hoque, Hira Fareed, Rbia Gilani",
+            students: "Eishika Hoque, Hira Fareed, & Rbia Gilani",
             link: "http://grocereasy.eishika.com/",
             keywords: ["app"]
         },
         {
             id: 11,
-            name: "Gaming Friend Finder",
-            description: "Love multiplayer games but don't have the squad?! Gaming Friend Finder can help... Built to connect like-minded gamers when looking for playing partners, this was an 8642 prototyping exercise with animated pitch materials..",
+            name: "Game Friend Finder",
+            description: "Game Friend Finder was built during an 8642 prototyping exercise and is designed to connect like-minded gamers.",
             image: "/static/images/projectImages/edited/gff.png",
             students: "Rob Cottignies, Drashya Gohil, Ankit Gupta, Andrew Block, & Padmini Smith",
             link: "http://www.robcottignies.com/projects/Gaming-Friend-Finder-Project",
             keywords: ["app"]
+        },
+        {
+            id: 12,
+            name: "Robot Space",
+            description: "Robot Space is a virtual reality First Person Shooter game built using Unity Technologies.",
+            image: "/static/images/projectImages/edited/robot.png",
+            students: "Tim Lu, Yan Zhang, & Flora Zuo",
+            link: "http://tim-lu-design.com/robot%20space/robot%20space.html",
+            keywords: ["ar/vr"]
         }
-
     ]
